793b06c6f33e5291ffd04c79036ca614e9a9a815
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-18 23:20:50 +0500

    Working fine, needs some error handling

--- index.js
+++ index.js
@@ -1,120 +1,58 @@
 const fs = require("fs");
 const http = require("http");
-let data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+//let data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const store = require("./store");
 
 const server = http.createServer((req, res) => {
 	if (req.url === "/todos" && req.method === "GET") {
-		getTodos(req, res);
+		handleTodos(req, res);
 	} else if (req.url === "/todos" && req.method === "POST") {
-		createTodo(req, res);
+		//createTodo(req, res);
+		handleCreateTodo(req, res);
 	} else if (req.url.match(/\/todos\/([0-9]+)/) && req.method === "GET") {
 		const id = req.url.split("/")[2];
-		getTodo(req, res, id);
+		//handleTodo(req, res, id);
+		HandleTodo(req, res, id);
 	} else if (req.url.match(/\/todos\/([0-9]+)/) && req.method === "DELETE") {
 		const id = req.url.split("/")[2];
-		deleteTodo(req, res, id);
+		//deleteTodo(req, res, id);
+		handleDeleteTodo(req, res, id);
 	} else if (req.url.match(/\/todos\/([0-9]+)/) && req.method === "PUT") {
 		const id = req.url.split("/")[2];
-		updateTodo(req, res, id);
+		//updateTodo(req, res, id);
+		handleUpdateTodo(req, res, id);
+	} else if (
+		req.url.match(/\/todos\/updateStatus\/([0-9]+)/) &&
+		req.method === "PUT"
+	) {
+		const id = req.url.split("/")[3];
+		handleChangeStatus(req, res, id);
 	} else {
-		res.writeHead(502, {"Content-Type": "application/json"});
+		res.writeHead(502, { "Content-Type": "application/json" });
 		res.end(JSON.stringify("Route not found"));
 	}
 });
 
 // Function to get all todos (GET REQUEST)
-function getTodos(req, res) {
+async function handleTodos(req, res) {
+	const todos = await store.getTodos();
 	res.writeHead(200, { "Content-Type": "application/json" });
-	res.end(JSON.stringify(data));
+	res.end(JSON.stringify(todos));
 }
 
 // Function to get a single todo (GET REQUEST)
-function getTodo(req, res, id) {
+async function HandleTodo(req, res, id) {
 	try {
-		// Search todo by id
-		let singleTodo;
-		for (let i = 0; i < data.length; i++) {
-			if (data[i].id === parseInt(id)) {
-				singleTodo = data[i];
-				break;
-			}
-		}
-		if (!singleTodo) {
+		let todo = await store.getTodo(id);
+		if (!todo) {
 			res.writeHead(404, { "Content-Type": "application/json" });
-			res.end(JSON.stringify(`Todo with id ${id} not found`));
+			res.end(JSON.stringify({ message: "Todo not found" }));
 		} else {
 			res.writeHead(200, { "Content-Type": "application/json" });
-			res.end(JSON.stringify(singleTodo));
+			res.end(JSON.stringify(todo));
 		}
 	} catch (err) {
 		console.log(err.message);
-		res.writeHead(503, { "Content-Type": "application/json" });
-		res.end("Internal Server Error");
-	}
-}
-
-// Function to call for post request
-function createTodo(req, res) {
-	try {
-		let body = "";
-		req.on("data", (chunk) => {
-			body += chunk;
-		});
-
-		req.on("end", () => {
-			const { title, description, isCompleted } = JSON.parse(body);
-			const getIdFromThere = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-			let id = getIdFromThere.length;
-			id++;
-			const newTodo = {
-				id: id,
-				title: title,
-				description: description,
-				isCompleted: isCompleted,
-			};
-			data.push(newTodo);
-			fs.writeFileSync("data.json", JSON.stringify(data));
-			res.writeHead(200, { "Content-Type": "application/json" });
-			res.write(JSON.stringify(newTodo));
-			res.end();
-		});
-	} catch (err) {
-		console.log(err.message);
-		res.writeHead(404, { "Content-Type": "application/json" });
-		res.end();
-	}
-}
-
-// Function to call for delete request
-function deleteTodo(req, res, id) {
-	try {
-		let indexOfTargetTodo;
-		if (!data[0]) {
-			res.writeHead(404, {"Content-Type": "application/json"});
-			res.end(JSON.stringify(`Todo list is empty`));
-		} else {
-			for (let i = 0; i < data.length; i++) {
-				if (data[i].id === parseInt(id)) {
-					indexOfTargetTodo = i;
-					break;
-				} else {
-					// not found
-				}
-			}
-			if (indexOfTargetTodo != 0 && !indexOfTargetTodo) {
-				res.writeHead(404, { "Content-Type": "application/json" });
-				res.end(JSON.stringify(`Todo with id ${id} not found`));
-			} else {
-				// Delete the todo with indexOfTargetTodo
-				data.splice(indexOfTargetTodo, 1);
-				fs.writeFileSync("data.json", JSON.stringify(data));
-				res.writeHead(200, { "Content-Type": "application/json" });
-				res.write(JSON.stringify(data));
-				res.end();
-			}
-		}
-	} catch (err) {
-		console.log(err);
 		res.writeHead(503, { "Content-Type": "application/json" });
 		res.end();
 	}
@@ -162,3 +100,80 @@ function updateTodo(req, res, id) {
 }
 
 server.listen(3000, () => console.log("Server is listening on 3000"));
+
+// Function to handle create todo request
+function handleCreateTodo(req, res) {
+	try {
+		let body = "";
+		req.on("data", (chunk) => {
+			body += chunk;
+		});
+		req.on("end", async () => {
+			const response = await store.createTodo(body);
+			res.writeHead(200, { "Content-Type": "application/json" });
+			res.end(JSON.stringify(response));
+		});
+	} catch (err) {
+		console.log(err);
+		res.writeHead(503, { "Content-Type": "application/json" });
+		res.end();
+	}
+}
+
+// Function to handle delete todo
+async function handleDeleteTodo(req, res, id) {
+	try {
+		let response = await store.deleteTodo(id);
+		if (response) {
+			res.writeHead(200, { "Content-Type": "application/json" });
+			res.end(JSON.stringify(response));
+		}
+		else {
+			res.writeHead(404, {"Content-Type": "application/json"});
+			res.end(JSON.stringify({message: "Todo not found"}));
+		}
+	} catch (err) {
+		res.writeHead(503, { "Content-Type": "text/plain" });
+		console.log(err);
+		const message = await store.deleteTodo(id);
+		res.end();
+	}
+}
+
+// Function to handle for changing status of todo
+function handleChangeStatus(req, res, id) {
+	try {
+		let body = "";
+		req.on("data", (chunk) => {
+			body += chunk;
+		});
+		req.on("end", async () => {
+			const response = await store.changeStatus(id, body);
+			res.writeHead(200, { "Content-Type": "application/json" });
+			res.end(JSON.stringify(response));
+		});
+	} catch (err) {
+		console.log(err);
+		res.writeHead(503, { "Content-Type": "application/json" });
+		res.end();
+	}
+}
+
+// Function to update todo
+function handleUpdateTodo(req, res, id) {
+	try {
+		let body = "";
+		req.on("data", (chunk) => {
+			body += chunk;
+		});
+		req.on("end", async () => {
+			const response = await store.updateTodo(id, body);
+			res.writeHead(200, { "Content-Type": "application/json" });
+			res.end(JSON.stringify(response));
+		});
+	} catch (err) {
+		console.log(err);
+		res.writeHead(503, { "Content-Type": "application/json" });
+		res.end();
+	}
+}
--- store.js
+++ store.js
@@ -1,1 +1,177 @@
-// All the saving, deleting, updating things will be write here.
+const fs = require("fs");
+const { resolve } = require("path");
+//const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+
+// Function to return all todos
+function getTodos() {
+	return new Promise((resolve, reject) => {
+		fs.readFile("data.json", "utf-8", (err, data) => {
+			if (err) {
+				console.log(err);
+				reject();
+			}
+			resolve(JSON.parse(data));
+		});
+	});
+}
+
+// Function to find a todo
+function getTodo(id) {
+	return new Promise((resolve, reject) => {
+		try {
+			fs.readFile("data.json", "utf-8", (err, data) => {
+				if (err) {
+					console.log(err);
+				}
+				data = JSON.parse(data);
+				let todo = "";
+				for (let i = 0; i < data.length; i++) {
+					if (data[i].id === parseInt(id)) {
+						todo = data[i];
+						break;
+					}
+				}
+				resolve(todo);
+			});
+		} catch (err) {
+			console.log(err.message);
+			reject("Todo not found");
+		}
+	});
+}
+
+// Function to create new todo
+function createTodo(body) {
+	return new Promise((resolve, reject) => {
+		try {
+			let todos = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+			let id = todos.length;
+			id++;
+			console.log(id);
+			const { title, description } = JSON.parse(body);
+			const todo = {
+				id: id,
+				title: title,
+				description: description,
+				isCompleted: false,
+			};
+			todos.push(todo);
+			fs.writeFile("data.json", JSON.stringify(todos), (err) => {
+				if (err) {
+					console.log(err);
+				}
+				// File writed successfull
+			});
+			resolve(todo);
+		} catch (err) {
+			console.log(err);
+		}
+	});
+}
+
+// Function to delete todo
+function deleteTodo(id) {
+	return new Promise((resolve, reject) => {
+		try {
+			let targetTodo = "";
+			fs.readFile("data.json", "utf-8", (err, data) => {
+				if (err) {
+					console.log(err);
+				}
+				data = JSON.parse(data);
+				for (let i = 0; i < data.length; i++) {
+					if (data[i].id === parseInt(id)) {
+						targetTodo = i;
+						break;
+					}
+				}
+				data.splice(targetTodo, 1);
+				fs.writeFile("data.json", JSON.stringify(data), (err) => {
+					if (err) {
+						console.log(err);
+					}
+					// file written successfully
+				});
+				resolve(data);
+			});
+		} catch (err) {
+			console.log(err);
+			reject();
+		}
+	});
+}
+
+// Function to change the status of todo, i.e., todo is completed or not
+function changeStatus(id, body) {
+	return new Promise((resolve, reject) => {
+		const { isCompleted } = JSON.parse(body);
+		body = JSON.parse(body);
+		fs.readFile("data.json", "utf-8", (err, data) => {
+			if (err) {
+				console.log(err);
+				reject();
+			}
+			data = JSON.parse(data);
+			let targetTodo;
+			for (let i = 0; i < data.length; i++) {
+				if (data[i].id === parseInt(id)) {
+					targetTodo = i;
+					break;
+				}
+			}
+			data[targetTodo].isCompleted = isCompleted;
+			fs.writeFile("data.json", JSON.stringify(data), (err) => {
+				if (err) {
+					console.log(err);
+					reject();
+				}
+				// Data writed
+			});
+			resolve(data[targetTodo]);
+		});
+	});
+}
+
+// Function to update todo
+function updateTodo(id, body) {
+	return new Promise((resolve, reject) => {
+		let { title, description } = JSON.parse(body);
+		let targetTodo;
+		fs.readFile("data.json", "utf-8", (err, data) => {
+			if (err) {
+				console.log(err);
+				reject();
+			}
+			data = JSON.parse(data);
+			for (let i = 0; i < data.length; i++) {
+				if (data[i].id === parseInt(id)) {
+					targetTodo = i;
+					break;
+				}
+			}
+			if (!targetTodo && targetTodo !== 0) {
+				reject("Todo not found");
+			} else {
+				data[targetTodo].title = title;
+				data[targetTodo].description = description;
+				fs.writeFile("data.json", JSON.stringify(data), (err) => {
+					if (err) {
+						console.log(err);
+						reject();
+					}
+					// File writed successfully
+				});
+				resolve(data[targetTodo]);
+			}
+		});
+	});
+}
+
+module.exports = {
+	getTodos,
+	getTodo,
+	createTodo,
+	deleteTodo,
+	changeStatus,
+	updateTodo,
+};
