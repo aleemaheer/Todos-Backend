261119bbb9c2a02f72dc01775567e410219656a9
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-25 16:03:59 +0500

    Updated backend, added cors, working fine

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const http = require("http");
 const store = require("./store");
 
 const server = http.createServer((req, res) => {
+	const headers = {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "OPTIONS, POST, GET, DELETE, PUT",
+		"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+		"Access-Control-Allow-Credentials": true
+	}
+
+	if (req.method === 'OPTIONS') {
+		res.writeHead(204, headers);
+		res.end();
+		return;
+	}
+
+	//if (['GET', 'POST'].indexOf(req.method) > -1) {
+	//	res.writeHead(200, headers);
+	//	res.end();
+	//	return;
+	//}
+
 	if (req.url === "/todos" && req.method === "GET") {
+		handleCors(req, res);
 		HandleTodos(req, res);
 	} else if (req.url === "/todos" && req.method === "POST") {
 		HandleCreateTodo(req, res);
@@ -128,27 +148,40 @@ function handleTodoStatus(req, res, id) {
 // Function to update status
 function handleUpdateTodo(req, res, id) {
 	try {
-		let body = '';
+		let body = "";
 		req.on("data", (chunk) => {
 			body += chunk;
 		});
-		req.on("end", async() => {
+		req.on("end", async () => {
 			const todos = new store.store(id, body);
 			const response = await todos.updateTodo();
 			if (!response) {
-				res.writeHead(404, {"Content-Type": "application/json"});
+				res.writeHead(404, { "Content-Type": "application/json" });
 				res.end(JSON.stringify("Todo not found"));
 			} else {
-				res.writeHead(200, {"Content-Type": "application/json"});
+				res.writeHead(200, { "Content-Type": "application/json" });
 				res.end(JSON.stringify(response));
 			}
-		})
-	} 
-	catch (err) {
+		});
+	} catch (err) {
 		console.log(err);
-		res.writeHead(503, {"Content-Type": "application/json"});
+		res.writeHead(503, { "Content-Type": "application/json" });
 		res.end();
 	}
+}
+
+function handleCors(req, res) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+		res.setHeader(
+			"Access-Control-Allow-Methods",
+			"POST, GET, PUT, DELETE,PATCH"
+		);
+		res.setHeader("Access-Control-Allow-Credentials", true);
+		res.statusCode = 200;
 }
 
 const PORT = process.env.PORT || 3000;
