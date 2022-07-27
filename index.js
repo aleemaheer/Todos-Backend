f5d487f005af5a8481edb81a8dc3ed02a16595df
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-27 09:26:49 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -185,4 +185,4 @@ function handleCors(req, res) {
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server is listening on 3000"));
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
