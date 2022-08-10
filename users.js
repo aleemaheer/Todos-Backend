2e966bb883a9965ca50367de819d138daa7c1012
aleem aheer <aleemaheer5769644@gmail.com>
2022-08-10 12:07:28 +0500

    Updated REST api, added getting password

--- users.js
+++ users.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
+const md5 = require("md5");
 //const path = "/home/aleemaheer/todos_backend/data";
-const path = require("path").join(process.cwd(), "data")
-console.log(path)
+const path = require("path").join(process.cwd(), "data");
+console.log(path);
 
 class User {
 	constructor() {
@@ -12,8 +13,7 @@ class User {
 		try {
 			const data = fs.readFileSync(path + "/users.json", "utf-8");
 			return JSON.parse(data);
-		}
-		catch (err) {
+		} catch (err) {
 			console.log(err);
 		}
 	}
@@ -28,49 +28,53 @@ class User {
 	}
 
 	// Register User
-	register(userName, email) {
+	register(userName, email, password) {
 		return new Promise(async (resolve, reject) => {
-				const data = await this.usersData();
-				let userId = data.length;
-				userId++;
-				let checkEmail = true;
-				if (data.length !== 0) {
-					// Check user's email that same emails cannot be created
-					for (let i = 0; i < data.length; i++) {
-						if (data[i].email === email) {
-							checkEmail = false;
-							break;
-						}
+			const data = await this.usersData();
+			let userId = data.length;
+			password = md5(password);
+			userId++;
+			let checkEmail = true;
+			if (data.length !== 0) {
+				// Check user's email that same emails cannot be created
+				for (let i = 0; i < data.length; i++) {
+					if (data[i].email === email) {
+						checkEmail = false;
+						break;
 					}
 				}
-				if (checkEmail) {
-					let newUser = {
-						userId: userId,
-						userName: userName,
-						email: email,
-					};
-					data.push(newUser);
-					fs.writeFile(path + "/users.json", JSON.stringify(data), (err) => {
-						if (err) {
-							console.log(err);
-							reject();
-						}
-						resolve(JSON.stringify(newUser));
-					});
-				} else {
-					resolve("emailNotAcceptable");
-				}
+			}
+			if (checkEmail) {
+				let newUser = {
+					userId: userId,
+					userName: userName,
+					email: email,
+					password,
+				};
+				data.push(newUser);
+				fs.writeFile(path + "/users.json", JSON.stringify(data), (err) => {
+					if (err) {
+						console.log(err);
+						reject();
+					}
+					// newUser = JSON.parse(newUser.splice(3, 1));
+					delete newUser.password;
+					resolve(JSON.stringify(newUser));
+				});
+			} else {
+				resolve("emailNotAcceptable");
+			}
 		});
 	}
 
 	// Login
-	login(email) {
+	login(email, password) {
 		return new Promise(async (resolve, reject) => {
 			try {
 				let userObject;
 				const data = await this.usersData();
 				for (let i = 0; i < data.length; i++) {
-					if (email === data[i].email) {
+					if (email === data[i].email && md5(password) === data[i].password) {
 						userObject = data[i];
 						break;
 					}
@@ -78,6 +82,7 @@ class User {
 				if (!userObject) {
 					resolve();
 				} else {
+					delete userObject.password;
 					resolve(JSON.stringify(userObject));
 				}
 			} catch (err) {
