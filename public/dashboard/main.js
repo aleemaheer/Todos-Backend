376ef10d111675296d542361e2813c39e14fe3b6
aleem aheer <aleemaheer5769644@gmail.com>
2022-08-24 17:47:46 +0500

    Added bootstrap and some more features

--- public/dashboard/main.js
+++ public/dashboard/main.js
@@ -4,17 +4,9 @@ const todoDescription = document.querySelector("#todoDescription");
 const todoStatus = document.querySelector("#todoStatus");
 const createTodoBtn = document.querySelector("#createTodo");
 const submitCreateTodoBtn = document.querySelector("#submitCreateTodo");
-//const updateTodoBtn = document.querySelector("#updateTodo");
-//const deleteTodoBtn = document.querySelector("#deleteTodo");
+const todoClass = document.querySelector(".todo");
+const tbody = document.querySelector("#tbody");
 const changePassword = document.querySelector("#changePassword");
-let updateTodoBtnIsActive = false;
-let deleteTodoBtnIsActive = false;
-
-// alert("helo");
-// submitCreateTodoBtn.style.display = "none";
-// todoTitle.style.display = "none";
-// todoDescription.style.display = "none";
-// todoStatus.style.display = "none";
 
 async function readTodos() {
 	try {
@@ -26,6 +18,9 @@ async function readTodos() {
 		const ol_todo = document.querySelector("#ol");
 		while (ol_todo.firstChild) ol_todo.removeChild(ol_todo.firstChild);
 
+		const tr = document.querySelector("#tr");
+		while (tbody.node) tr.removeChild(tbody.node);
+
 		const result = await fetch("http://localhost:3000/todos", {
 			method: "GET",
 			headers: {
@@ -34,13 +29,64 @@ async function readTodos() {
 		});
 		const todos = await result.json();
 		todos.forEach((t) => {
-			const li = document.createElement("li");
-			const button = document.createElement("button");
-			li.innerHTML = `<h3>Title:</h3> ${t.title} <h4>Description:</h4> ${t.description} <h4>isCompleted:</h4> ${t.is_completed}`;
-			li.id = t.todo_id;
-			// Delete todo with onclick
-			li.addEventListener("click", async (e) => {
+			const tdOfEdit = document.querySelector("#editBtn");
+			const tdOfDelete = document.querySelector("#deleteBtn");
+			const tdOfStatus = document.querySelector("#editStatusBtn");
+			const tdOfTitle = document.querySelector("#title");
+			const tdOfDescription = document.querySelector("#description");
+			const wTdOfStatus = document.querySelector("#status");
+			const editButton = document.createElement("button");
+			const deleteButton = document.createElement("button");
+			const editStatusButton = document.createElement("button");
+			const tr = document.createElement("tr");
+			//tr.innerHTML = `<th>${t.title}</th><td>${t.description}</td><td>${t.is_completed}</td><td><button>Edit ✍️</button></td><td><button>Delete ❌</button></td><td><button>Edit Status ✅</button></td>`;
+			editButton.innerText = "Edit ✍️";
+			deleteButton.innerText = "Delete ❌";
+			editStatusButton.innerText = "Edit Status ✅";
+
+			editButton.id = t.todo_id;
+			deleteButton.id = t.todo_id;
+			editStatusButton.status = t.is_completed;
+			editStatusButton.id = t.todo_id;
+
+			// Append in table
+			tdOfEdit.appendChild(editButton);
+			tdOfDelete.appendChild(deleteButton);
+			tdOfStatus.appendChild(editStatusButton);
+			tdOfTitle.innerText = t.title;
+			tdOfDescription.innerText = t.description;
+			wTdOfStatus.innerText = t.is_completed;
+
+			editButton.addEventListener("click", (e) => {
+				window.location = `/dashboard/edit.html/id=${e.target.id}+${userId}`;
+			});
+			editStatusButton.addEventListener("click", async (e) => {
+				let status;
+				if (e.target.status === true) {
+					status = false;
+				} else {
+					status = true;
+				}
 				const json_request = {};
+				json_request.isCompleted = status;
+				console.log(json_request);
+				const result = await fetch(
+					`http://localhost:3000/todos/${e.target.id}`,
+					{
+						method: "PATCH",
+						headers: {
+							"content-type": "application/json",
+							user_id: userId,
+						},
+						body: JSON.stringify(json_request),
+					}
+				);
+				const response = await result.json();
+				console.log(response);
+				window.location = window.location.href;
+			});
+			// Delete todo with onclick
+			deleteButton.addEventListener("click", async (e) => {
 				const result = await fetch(
 					`http://localhost:3000/todos/${e.target.id})`,
 					{
@@ -54,8 +100,8 @@ async function readTodos() {
 				const success = await result.json();
 				readTodos();
 				alert("Deleted");
+				window.location = window.location.href;
 			});
-			ol_todo.appendChild(li);
 		});
 	} catch (e) {
 		console.log(e);
